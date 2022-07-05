727e9689d565c4fa15de1ae936c27c2b9e1acc17
何喜阳 <iamxiyang@foxmail.com>
2022-07-05 23:15:53 +0800

    feat: 增加重命名保存到其他目录

--- src/replaceName.ts
+++ src/replaceName.ts
@@ -1,14 +1,19 @@
-import fs from 'fs'
-import { parse } from 'pathe'
+import fse from 'fs-extra'
+import { parse, resolve } from 'pathe'
 import { red } from 'colorette'
+import { Output } from './types'
 
 // 修改文件名
-export default async (path: string, name: string) => {
-  const { dir, ext } = parse(path)
-  fs.rename(path, `${dir}/${name}${ext}`, (err) => {
-    if (err) {
-      console.error(red(path + '修改出错'), err)
-      return
+export default async (oldPath: string, name: string, output: Output) => {
+  try {
+    const { dir, ext } = parse(oldPath)
+    const newPath = resolve(dir, output.path + '', name + ext)
+    if (dir !== resolve(dir, output.path + '')) {
+      fse.copySync(oldPath, newPath)
+    } else {
+      fse.renameSync(oldPath, newPath)
     }
-  })
+  } catch (err) {
+    console.error(red('修改出错:' + oldPath), err)
+  }
 }
